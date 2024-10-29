58c8da4d0ec2fac80114f8963eea73c9f8cde4c1
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2024-10-29 17:52:51 +1100

    Update Ch2.1.lean

---
+++ Theorem Proving in lean/Chapter 2/Ch2.1.lean
@@ -0,0 +1,65 @@
+import Mathlib.Data.Nat.Basic
+import Mathlib.Data.Nat.Parity
+import MIL.Common
+
+
+theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
+  fun hpq : p ∧ q =>
+  have hp : p := And.left hpq
+  have hq : q := And.right hpq
+  show q ∧ p from And.intro hq hp
+
+
+  /- Define some constants. -/
+
+def m : Nat := 1       -- m is a natural number
+def n : Nat := 0
+def b1 : Bool := true  -- b1 is a Boolean
+def b2 : Bool := false
+def k: Int := -3
+def p: Int := k*m
+/- Check their types. -/
+def f: Nat -> Int := -1
+#check f
+#check p
+#check k
+#check m            -- output: Nat
+#check n
+#check n + 0        -- Nat
+#check m * (n + 0)  -- Nat
+#check b1           -- Bool
+#check b1 && b2     -- "&&" is the Boolean and
+#check b1 || b2     -- Boolean or
+#check true         -- Boolean "true"
+
+
+
+
+
+
+#check Nat → Nat      -- type the arrow as "\to" or "\r"
+#check Nat -> Nat     -- alternative ASCII notation
+
+#check Nat × Nat      -- type the product as "\times"
+#check Prod Nat Nat   -- alternative notation
+
+#check Nat → Nat → Nat
+#check Nat → (Nat → Nat)  --  same type as above
+
+#check Nat × Nat → Nat
+#check (Nat → Nat) → Nat -- a "functional"
+
+#check Nat.succ     -- Nat → Nat
+#check (0, 1)       -- Nat × Nat
+#check Nat.add      -- Nat → Nat → Nat
+
+#check Nat.succ 2   -- Nat
+#check Nat.add 3    -- Nat → Nat
+#check Nat.add 5 2  -- Nat
+#check (5, 9).1     -- Nat
+#check (5, 9).2     -- Nat
+
+#eval Nat.succ 2   -- 3
+#eval Nat.add 5 2  -- 7
+#eval (5, 9).1     -- 5
+#eval (5, 9).2     -- 9
