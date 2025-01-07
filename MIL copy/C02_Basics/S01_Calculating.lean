27d65fb61ebbf8e797a9785f63dcf7ee723f07e2
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-01-07 22:29:27 +1100

    Update S01_Calculating.lean

--- MIL copy/C02_Basics/S01_Calculating.lean
+++ MIL copy/C02_Basics/S01_Calculating.lean
@@ -1,38 +1,53 @@
-import MIL.Common
+
+import Mathlib.Tactic
+import Mathlib.Util.Delaborators
 import Mathlib.Data.Real.Basic
+
+set_option warningAsError false
 -- An example.
 example (a b c : ℝ) : a * b * c = b * (a * c) := by
   rw [mul_comm a b]
   rw [mul_assoc b a c]
 
-
-
-
-
-theorem test (a b c: ℕ+) : a * b * c = b * (a * c) :=
-  have hc: a*b*c = b*a*c := mul_right_cancel_iff.mpr (mul_comm a b)
-  sorry
-
-variable (m n k: ℕ+)
-variable (a b c: ℝ)
-#check mul_right_cancel_iff.mpr
--- mul_right_cancel_iff.mpr : ?m.1063 = ?m.1064 → ?m.1063 * ?m.1062 = ?m.1064 * ?m.1062
-#check mul_right_cancel_iff.mpr (mul_comm m n)
-#check mul_right_cancel_iff.mpr
---#check mul_right_cancel_iff.mpr (mul_comm a b)
-#check mul_comm m (n*k)
-#check mul_assoc
-
 -- Try these.
+-- conv version
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  rw [mul_assoc c b a]
-  rw [mul_comm c (b*a)]
-  rw [mul_assoc b a c]
+  conv =>
+  rhs
+  rw [←mul_assoc,mul_comm,←mul_assoc]
 
+--term proof
+example (a b c : ℝ) : c * b * a = b * (a * c) :=
+calc
+c*b*a = c*(b*a) := mul_assoc c b a
+_ = (b*a)*c := mul_comm c (b*a)
+_ = b*a*c := rfl
+_ = b*(a*c) := mul_assoc b a c
+
+example (a b c : ℝ) : c * b * a = b * (a * c) := by
+ rw [mul_comm c,mul_comm a,←mul_assoc]
+
+--conv proof
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  rw [←mul_assoc a b c]
-  rw [mul_comm a b]
-  rw [mul_assoc b a c]
+  conv =>
+  rhs
+  rw [←mul_assoc]
+  rw [mul_comm b a]
+  exact mul_assoc a b c
+
+
+
+example (a b c : ℝ) : a * (b * c) = b * (a * c) :=
+calc
+ a*(b*c) = a*b*c := Eq.comm.mp (mul_assoc a b c)
+ _ = b*a*c := by rw [mul_comm a b]
+ _ = b*(a*c) := mul_assoc b a c
+
+
+
+
+
+
 
 
 -- An example.
@@ -40,55 +55,29 @@ example (a b c : ℝ) : a * b * c = b * c * a := by
   rw [mul_assoc]
   rw [mul_comm]
 
-
-
-
 /- Try doing the first of these without providing any arguments at all,
    and the second with only one argument. -/
 example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
-  rw [mul_comm]
-  rw [mul_assoc]
+  rw [mul_comm,mul_assoc]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  rw [←mul_assoc]
-  rw [mul_comm a ] -- just one argument, matching first pattern.
-  rw [mul_assoc]
+  rw [mul_comm a,mul_comm a,mul_assoc]
 
 -- Using facts from the local context.
---v1
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h']
   rw [← mul_assoc]
   rw [h]
   rw [mul_assoc]
-
---v2
-example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
-  rw [← mul_assoc]
-  rw [h]
-  rw [mul_assoc]
-  rw [h']
-
-
-
 
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  rw [mul_assoc a b c]
-  rw [h]
-  rw [← mul_assoc]
-
+  rw [mul_assoc a,h,←mul_assoc a]
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  rw [hyp,hyp',mul_comm,sub_self]
+  rw [hyp,hyp',mul_comm b,sub_self]
 
---v1
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
-
---v2
-example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
-  rw [← mul_assoc, h', h,mul_assoc]
-
 
 section
 
@@ -110,9 +99,6 @@ variable (a b c : ℝ)
 #check mul_assoc c a b
 #check mul_comm a
 #check mul_comm
-#check mul_assoc a
-#synth LinearOrder Nat
-
 
 end
 
@@ -138,72 +124,37 @@ example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
     (a + b) * (a + b) = a * a + b * a + (a * b + b * b) := by
       rw [mul_add,add_mul,add_mul]
     _ = a * a + (b * a + a * b) + b * b := by
-      rw [ ← add_assoc, add_assoc (a * a) (b * a) (a * b)]
+      rw [←add_assoc, add_assoc (a*a)]
     _ = a * a + 2 * (a * b) + b * b := by
-      rw [mul_comm a b,← two_mul]
+      rw [mul_comm b a, ← two_mul]
 
 end
 
 -- Try these. For the second, use the theorems listed underneath.
 section
 variable (a b c d : ℝ)
-variable (p₂:b*a  = b*a)
- --v1
-example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  rw [mul_add,add_mul,add_mul,← add_assoc,add_assoc (a * c) (b * c),add_comm (b * c),← add_assoc]
 
---v2
-example : (a + b) * (c + d) = a * c + a * d + b * c + b * d :=
-calc
-  (a + b) * (c + d) = (a + b) * c + (a + b)*d := by rw [left_distrib]
-  _ = a*c + b*c + (a + b)*d := by rw [right_distrib]
-  _ = a*c + b*c + (a*d + b*d) := by rw [right_distrib]
-  _ = a*c + b*c + a*d + b*d := by rw [← add_assoc]
-  _ = a*c + (b*c + a*d) + b*d := by rw [add_assoc (a*c)]
-  _ = a*c + (a*d + b*c) + b*d := by rw [add_comm (a*d)]
-  _ = a*c + a*d + b*c + b*d := by rw [← add_assoc]
+theorem m1 : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
+  rw [mul_add,add_mul,add_mul,←add_assoc,add_assoc (a*c),add_comm (b*c),←add_assoc]
 
-
---v1 via apply?
 example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  exact Eq.symm (sq_sub_sq a b)
-
---v2 made up improv
-example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  rw [right_distrib,sub_eq_add_neg,left_distrib,left_distrib,← add_assoc,mul_neg,mul_comm a b,add_assoc (a*a),add_comm (-(b*a)),← sub_eq_add_neg,sub_eq_zero.mpr,add_zero,mul_neg,← sub_eq_add_neg,← pow_two, ← pow_two]
-  simp
-
---v3 neater no calc
-example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-rw [right_distrib,mul_sub_left_distrib,mul_sub_left_distrib]
-rw [sub_eq_add_neg,sub_eq_add_neg,← add_assoc]
-rw [mul_comm a b,← sub_eq_add_neg]
-rw [add_assoc (a*a), add_comm (-(b * a)),← sub_eq_add_neg]
-rw [←pow_two,←pow_two]
-rw [sub_self,add_zero]
+  have h1: a-b = a+(-b) := rfl;
+  rw [h1,m1,mul_neg,mul_neg];
+  rw [←pow_two,←pow_two]
+  rw [mul_comm b a,add_assoc (a^2),add_comm (-(a*b)),add_neg_cancel,add_zero]
+  rfl
 
 
-
-def p₁: Prop := ((b*a) = (b*a))
-variable (p₂:b*a  = b*a)
-#check b*a  = b*a
-#check p₁ a b
+#check add_neg_cancel
 #check pow_two a
 #check mul_sub a b c
 #check add_mul a b c
 #check add_sub a b c
 #check sub_sub a b c
 #check add_zero a
---
-#check sub_eq_add_neg
 #check mul_neg
-#check sub_eq_zero
-#check sub_eq_zero.mpr p₂
-#check sub_zero
-
 
 end
-
 
 -- Examples.
 
