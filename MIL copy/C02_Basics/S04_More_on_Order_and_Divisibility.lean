b59a2f5a183585eb99cf10a8f0719c584474cea2
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-01-08 17:53:32 +1100

    Update S04_More_on_Order_and_Divisibility.lean

--- MIL copy/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL copy/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -1,11 +1,14 @@
-import MIL.Common
+import Mathlib.Tactic
+import Mathlib.Util.Delaborators
 import Mathlib.Data.Real.Basic
 
 namespace C02S04
 
 section
 variable (a b c d : ℝ)
-
+#print min --doesnt have order in it?
+#print Min
+#print min_le_right
 #check (min_le_left a b : min a b ≤ a)
 #check (min_le_right a b : min a b ≤ b)
 #check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
@@ -29,7 +32,7 @@ example : min a b = min b a := by
     apply min_le_left
   apply le_antisymm
   apply h
-  apply h
+  apply h --swaps the arguments
 
 example : min a b = min b a := by
   apply le_antisymm
@@ -38,20 +41,86 @@ example : min a b = min b a := by
     apply min_le_right
     apply min_le_left
 
+
+variable (x:ℝ)
+
+#check max_le_max_right
+#check max_le_max_right (h:= le_refl x)
+#check max_le_max_left (h:= le_refl x)
+#check le_max_left
+#check le_max_right
+
+
+#check (min_le_left a b : min a b ≤ a)
+#check (min_le_right a b : min a b ≤ b)
+#check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
+#check max_le --∀ {α : Type u_1} [inst : LinearOrder α] {a b c : α}, a ≤ c → b ≤ c → max a b ≤ c
+
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  . have h11: max b a ≥ a ∧ max b a ≥ b := And.intro (le_max_right b a) (le_max_left b a)
+    exact max_le (c:=max b a) h11.1 h11.2
+  . have h1: max a b ≥ b ∧ max a b ≥ a := And.intro (le_max_right a b) (le_max_left a b)
+    exact max_le (c:=max a b) h1.1 h1.2
+
+#check min_cases
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  rw [min_comm a (min b c)];
+  cases min_cases (min b c) a
+  . case inl h => rw [h.1]; apply (min_le_iff.mp h.2).elim; intro hp; rw [min_eq_right hp];intro hp;admit
+
+
+
+#check min_le_left  (b + c) (a + c)
+#check min_eq_left
+#check min_eq_right
+#check min_le_iff.mp (k:min b c ≤ a)
+
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+ cases min_cases a b
+ . case inl h =>
+    rw [h.1];
+    let (h1: (a + c) ≤ (b + c)) := add_le_add (h.2) (le_refl c);
+    rw [min_eq_left]; exact h1
+ . case inr h =>
+     rw [h.1];
+     let (h2: (b + c) ≤ (a + c)) := by linarith;
+     rw [min_eq_right]; exact h2
+
+
+#check add_le_add (_:a ≤ b) (le_refl c)
+#check aux
+#check aux a b (b - a) --aux a b (b - a) : min a b + (b - a) ≤ min (a + (b - a)) (b + (b - a))
+
+--aux a b (b - a) : min a b + (b - a) ≤ min (a + (b - a)) (b + (b - a))
+--aux a b (b - a) : min a b + (b - a) ≤ min b (b + (b - a))
+-- min a b + b ≤ min b (b + (b - a)) + a
+
+#check add_neg_cancel_right
+
+
+
+
+
 example : min a b + c = min (a + c) (b + c) := by
   sorry
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
-end
 
+#check add_sub_cancel_right
+
+
+example : |a| - |b| ≤ |a - b| :=
+  calc |a| - |b| = |a + b - b| - |b| := by rw [add_sub_cancel_right a b]
+  _ ≤ |a-b| + |b| -|b| := by rw [add_comm,←add_sub]; linarith [abs_add b (a-b)]
+  _ ≤ |a-b| := by rw [←add_sub];simp
+
+
+
+
+#check add_sub
 section
 variable (w x y z : ℕ)
 
@@ -63,12 +132,20 @@ example : x ∣ y * x * z := by
   apply dvd_mul_left
 
 example : x ∣ x ^ 2 := by
-   apply dvd_mul_left
+  apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
-end
+  apply dvd_add
+  . apply dvd_add
+    . rw [←mul_assoc y x z,mul_comm y,mul_assoc]; exact dvd_mul_right x (y*z)
+    . apply dvd_mul_left
+  . rw [pow_two w];apply dvd_mul_of_dvd_left h w
 
+#check Nat.pow_two
+#check dvd_mul_of_dvd_left  (_ : x ∣ w) w
+#check dvd_mul_left x (y*z)
+#check dvd_add
+end
 section
 variable (m n : ℕ)
 
@@ -80,5 +157,3 @@ variable (m n : ℕ)
 example : Nat.gcd m n = Nat.gcd n m := by
   sorry
 end
-
-
