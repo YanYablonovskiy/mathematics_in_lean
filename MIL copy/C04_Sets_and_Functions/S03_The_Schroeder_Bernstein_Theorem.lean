df0452275910cca62083533e5deb1bf473d0bde5
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-01-20 18:37:43 +1100

    Update S03_The_Schroeder_Bernstein_Theorem.lean

--- MIL copy/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
+++ MIL copy/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
@@ -32,6 +32,10 @@ def sbSet :=
 def sbFun (x : α) : β :=
   if x ∈ sbSet f g then f x else invFun g x
 
+
+#check invFun
+#print invFun
+
 theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
   have : x ∈ g '' univ := by
     contrapose! hx
@@ -42,8 +46,11 @@ theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
     . trivial
     . exact hx
   have : ∃ y, g y = x := by
-    sorry
-  sorry
+    simp at this
+    exact this
+  rw [invFun]
+  rw [dif_pos this,choose_spec this]
+
 
 section
 variable (g : β → α) (x : α)
@@ -52,14 +59,17 @@ variable (g : β → α) (x : α)
 #check (leftInverse_invFun : Injective g → LeftInverse (invFun g) g)
 #check (leftInverse_invFun : Injective g → ∀ y, invFun g (g y) = y)
 #check (invFun_eq : (∃ y, g y = x) → g (invFun g x) = x)
-
+#check (RightInverse)
+#print RightInverse
 end
+#check dif_pos
+#check if_neg
 
 theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
   set A := sbSet f g with A_def
   set h := sbFun f g with h_def
   intro x₁ x₂
-  intro (hxeq : h x₁ = h x₂)
+  intro (hxeq : h x₁ = h x₂) --to prove h is injective, so show x₁ = x₂
   show x₁ = x₂
   simp only [h_def, sbFun, ← A_def] at hxeq
   by_cases xA : x₁ ∈ A ∨ x₂ ∈ A
@@ -72,15 +82,59 @@ theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
       rw [if_pos x₁A, if_neg x₂nA] at hxeq
       rw [A_def, sbSet, mem_iUnion] at x₁A
       have x₂eq : x₂ = g (f x₁) := by
-        sorry
+        rw [hxeq]
+        apply Eq.comm.mp
+        exact sb_right_inv f g x₂nA
       rcases x₁A with ⟨n, hn⟩
       rw [A_def, sbSet, mem_iUnion]
       use n + 1
       simp [sbAux]
       exact ⟨x₁, hn, x₂eq.symm⟩
-    sorry
+    rw [if_pos x₁A,if_pos x₂A] at hxeq
+    exact hf hxeq
   push_neg at xA
-  sorry
+  rcases xA with ⟨x₁nA,x₂nA⟩
+  rw [if_neg x₁nA,if_neg x₂nA] at hxeq
+  have t1:_ := sb_right_inv f g x₁nA
+  have t2:_ := sb_right_inv f g x₂nA
+  have t3: ∃ x, g x = x₂ := by use invFun g x₂
+  have t4: ∃ x, g x = x₁ := by use invFun g x₁
+  have := Eq.refl (g (invFun g x₁))
+  have t5: g (invFun g x₁) = g (invFun g x₂) := by nth_rewrite 2 [hxeq] at this;exact this
+  rw [t1,t2] at t5
+  exact t5
+
+
+
+
+#check invFun_eq -- (∃ y, g y = x) → g (invFun g x) = x)
+#check invFun_eq_of_injective_of_rightInverse
+
+/-
+{f : α → β}
+{g : β → α} (hf : Injective f) (hg : RightInverse g f) : invFun f = g
+-/
+
+
+
+
+/-
+  rw [if_neg xA.1,if_neg xA.2] at hxeq
+  rw [invFun,invFun] at hxeq
+  have t1:_ := sb_right_inv f g xA.1
+  have t2:_ := sb_right_inv f g xA.2
+  have t3: ∃ x, g x = x₂ := by use invFun g x₂
+  have t4:  ∃ x, g x = x₁ := by use invFun g x₁
+  have t5: ∃ x, g x = x₂ := by use invFun g x₂
+  have t6:  ∃ x, g x = x₁ := by use invFun g x₁
+  obtain ⟨x₁nA,x₂nA⟩ := xA
+  rw [dif_pos t4] at hxeq
+  rw [dif_pos t3] at hxeq
+  obtain ⟨x1,x1eq⟩ := t3
+  obtain ⟨x2,x2eq⟩ := t4
+  rw [Eq.comm.mp x1eq,Eq.comm.mp x2eq]
+-/
+
 
 theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -99,7 +153,17 @@ theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
       exact ⟨n, xmem⟩
     simp only [h_def, sbFun, if_pos this]
     exact hg hx
-  sorry
+  have := sb_right_inv (β:=β) f g gyA
+  have := hg this
+  use g y
+  rw [h_def]
+  rw [sbFun]
+  rw [if_neg gyA]
+  exact this
+
+
+
+
 
 end
 
