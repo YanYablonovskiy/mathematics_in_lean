d6c3de4185a8dd8d131718b5db872cdaa7e10c3f
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-01-24 00:11:36 +1100

    Update S02_Induction_and_Recursion.lean

--- MIL copy/C05_Elementary_Number_Theory/S02_Induction_and_Recursion.lean
+++ MIL copy/C05_Elementary_Number_Theory/S02_Induction_and_Recursion.lean
@@ -1,6 +1,7 @@
 import Mathlib.Data.Nat.GCD.Basic
 import Mathlib.Tactic
 import Mathlib.Util.Delaborators
+import Mathlib.Data.Set.Lattice
 
 example (n : Nat) : n.succ ≠ Nat.zero :=
   Nat.succ_ne_zero n
@@ -103,7 +104,7 @@ theorem pow_two_le_fac (n : ℕ) : 2 ^ (n - 1) ≤ fac n := by
   rcases n with _ | n
   · simp [fac]
   . simp [fac]
-    induction n with
+    induction n with --now its the n in the factorial ; but this is the lean original induction way
     | zero => simp [fac]
     | succ n ih =>
       simp [fac]
@@ -116,7 +117,21 @@ theorem pow_two_le_fac (n : ℕ) : 2 ^ (n - 1) ≤ fac n := by
       . simp
       . simp
 
-
+example (n : ℕ):  2 ^ (n-1) ≤ fac n := by
+rcases n with _ | n
+. simp [fac]
+. simp [fac] --now its easier since n is not 0.
+  induction' n with j ih
+  . simp [fac]
+  . simp [fac]
+    rw [pow_succ]
+    have: 2 ≤ j + 1 + 1 := by linarith
+    rw [mul_comm (j+1+1)]
+    apply mul_le_mul_of_nonneg (a:=2^j) (d:= (j+1+1))
+    . exact ih
+    . exact this
+    . simp
+    . simp
 
 section
 
@@ -143,7 +158,7 @@ example : (range n).prod f = ∏ x in range n, f x :=
 example (f : ℕ → ℕ) : ∑ x in range 0, f x = 0 :=
   Finset.sum_range_zero f
 
-example (f : ℕ → ℕ) (n : ℕ) : ∑ x in range n.succ, f x = ∑ x in range n, f x + f n :=
+example (f : ℕ → ℕ) (n : ℕ) : ∑ x in range n.succ, f x = ∑ x in range n, f x + f n := --range n is {0,1,2...,n-1}. So when n+1, its {0,1,2,..,n-1,n}, hence f x + f n
   Finset.sum_range_succ f n
 
 example (f : ℕ → ℕ) : ∏ x in range 0, f x = 1 :=
@@ -153,23 +168,69 @@ example (f : ℕ → ℕ) (n : ℕ) : ∏ x in range n.succ, f x = (∏ x in ran
   Finset.prod_range_succ f n
 
 example (n : ℕ) : fac n = ∏ i in range n, (i + 1) := by
-  induction' n with n ih
+  induction' n with j ih
   · simp [fac, prod_range_zero]
-  simp [fac, ih, prod_range_succ, mul_comm]
+  simp only [fac]
+  simp only [prod_range_succ] -- (j + 1) * fac j = (∏ i ∈ range j, (i + 1)) * (j + 1)
+  rw [←ih]
+  apply mul_comm
+  --simp [fac, ih]
+  --simp [prod_range_succ, mul_comm]
 
 example (a b c d e f : ℕ) : a * (b * c * f * (d * e)) = d * (a * f * e) * (c * b) := by
   simp [mul_assoc, mul_comm, mul_left_comm]
 
 theorem sum_id (n : ℕ) : ∑ i in range (n + 1), i = n * (n + 1) / 2 := by
-  symm; apply Nat.div_eq_of_eq_mul_right (by norm_num : 0 < 2)
+  symm; apply Nat.div_eq_of_eq_mul_right (by norm_num : 0 < 2) --symm is the same as Eq.comm.mp (for any commutative goal)
   induction' n with n ih
   · simp
   rw [Finset.sum_range_succ, mul_add 2, ← ih]
   ring
+#check div_mul_cancel
+#check Nat.div_mul_cancel
+#check Nat.mod_two_eq_zero_or_one
+#check Nat.add_div
+open Set
+
+#check Nat.mod_two_eq_zero_or_one
+#check Nat.div_self
+#check Nat.mul_one
+#check Nat.mul_div_cancel_left
+#check Nat.add_assoc
+#check Nat.sub_eq_iff_eq_add --Nat.sub_eq_iff_eq_add {b a c : ℕ} (h : b ≤ a) : a - b = c ↔ a = c + b
+#check Nat.add_div_of_dvd_right
+#check Nat.mul_div_cancel'
+#check Nat.div_le_of_le_mul
+#check Nat.mul_div_assoc
+#check Nat.add_left_cancel_iff
 
 theorem sum_sqr (n : ℕ) : ∑ i in range (n + 1), i ^ 2 = n * (n + 1) * (2 * n + 1) / 6 := by
-  sorry
-end
+apply Eq.comm.mp
+induction' n with j ih
+. simp
+. rw [Finset.sum_range_succ,←ih]
+  simp [mul_add,add_mul,←pow_two]
+  ring_nf
+  rw [←add_comm ((j + j ^ 2 * 3 + j ^ 3 * 2) / 6)]
+  have: (6 + j * 13 + j ^ 2 * 9 + j ^ 3 * 2) = (6*(1+j*2 + j ^ 2) + (j + j^2*3 + (j^ 3)*2)) := by simp_arith
+  rw [this]
+  rw [Nat.add_div_of_dvd_right (a:=6 * (1 + j * 2 + j ^ 2))]
+  rw [add_comm]
+  rw [propext (Nat.add_left_cancel_iff)]
+  simp
+  use (1 + j * 2 + j ^ 2)
+
+
+
+
+
+
+
+  example:(@Nat.div_self 6 (by norm_num)) = (h:6/6 = 1) := by
+   rfl
+
+
+
 
 inductive MyNat where
   | zero : MyNat
