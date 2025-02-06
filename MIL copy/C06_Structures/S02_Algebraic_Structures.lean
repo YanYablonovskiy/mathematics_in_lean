35bbebd8534edbb1d7794e4e2b73a8b4d42f3408
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-02-06 16:55:44 +1100

    Update S02_Algebraic_Structures.lean

--- MIL copy/C06_Structures/S02_Algebraic_Structures.lean
+++ MIL copy/C06_Structures/S02_Algebraic_Structures.lean
@@ -193,6 +193,16 @@ def addGroupPoint : AddGroup₁ Point :=
 end Point
 
 section
+/-
+We are making progress. Now we know how to define algebraic structures in Lean, and we know how to define instances of those structures.
+
+But we also want to associate notation with structures so that we can use it with each instance.
+
+Moreover, we want to arrange it so that we can define an operation on a structure and use it with any particular instance,
+and we want to arrange it so that we can prove a theorem about a structure and use it with any instance.
+
+In fact, Mathlib is already set up to use generic group notation, definitions, and theorems for Equiv.Perm α.
+-/
 variable {α : Type*} (f g : Equiv.Perm α) (n : ℕ)
 
 #check f * g
@@ -235,7 +245,31 @@ def mySquare {α : Type*} [Group₂ α] (x : α) :=
   Group₂.mul x x
 
 #check mySquare
+/-
+The #check command shows that Group₂.mul has an implicit argument [Group₂ α] that we expect to be found by class inference,
+where α is the type of the arguments to Group₂.mul.
 
+In other words, {α : Type*} is the implicit argument for the type of the group elements and [Group₂ α]
+is the implicit argument for the group structure on α.
+
+Similarly, when we define a generic squaring function my_square for Group₂, we use an implicit argument {α : Type*}
+for the type of the elements and an implicit argument [Group₂ α] for the Group₂ structure.
+
+In the first example, when we write Group₂.mul f g, the type of f and g tells Lean that in the argument α to Group₂.mul
+has to be instantiated to Equiv.Perm β. That means that Lean has to find an element of Group₂ (Equiv.Perm β).
+
+The previous instance declaration tells Lean exactly how to do that. Problem solved!
+
+This simple mechanism for registering information so that Lean can find it when it needs it is remarkably useful.
+Here is one way it comes up. In Lean’s foundation, a data type α may be empty.
+
+In a number of applications, however, it is useful to know that a type has at least one element.
+
+For example, the function List.headI, which returns the first element of a list, can return the default value when the list is empty.
+
+ To make that work, the Lean library defines a class Inhabited α,
+ which does nothing more than store a default value. We can show that the Point type is an instance:
+-/
 section
 variable {β : Type*} (f g : Equiv.Perm β)
 
@@ -284,7 +318,41 @@ def foo : f * 1 * g⁻¹ = g.symm.trans ((Equiv.refl α).trans f) :=
   rfl
 
 end
+/-
+As a similarly artificial exercise, define a class AddGroup₂ in analogy to Group₂.
 
+Define the usual notation for addition, negation, and zero on any AddGroup₂ using the classes Add, Neg, and Zero.
+
+Then show Point is an instance of AddGroup₂.
+
+Try it out and make sure that the additive group notation works for elements of Point.
+-/
 class AddGroup₂ (α : Type*) where
   add : α → α → α
   -- fill in the rest
+  (zero: α)
+  (neg: α → α)
+  (zero_add : ∀a:α,add a zero = a)
+  (add_zero : ∀a:α,add zero a = a)
+  (neg_add_cancel : ∀a:α,add a (neg a) = zero)
+  (add_assoc : ∀(a b c:α),add a (add b c) = add (add a b) c)
+
+instance: AddGroup₂ Point where
+ add := Point.add
+ zero := default
+ neg := Point.neg
+ zero_add := by rw [default,instInhabitedPoint];simp [Point.add]
+ add_zero := by rw [default,instInhabitedPoint];simp [Point.add]
+ neg_add_cancel := by simp [Point.neg,Point.add]; rfl
+ add_assoc := by simp [Point.add,add_assoc]
+
+variable (x y: Point)
+
+#check x+y
+
+
+instance : Neg Point where neg := Point.neg
+instance : Zero Point where zero := default
+
+#check x+0
+#check x+(-y)+-0
